ac96899fb649b123d473fb074365fdb627552332
Luan Dinh <luan.dinh@shoalgroup.com>
2026-08-01 21:50:36 +1000

    finish chapter 1

---
+++ chapter_1_basics/chapter_1_quiz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+int input_1 {};
+int input_2 {};
+
+std::cout << "Enter an integer: ";
+std::cin >> input_1;
+
+std::cout << "Enter another integer: "; 
+std::cin >> input_2;
+
+// solution
+std::cout << input_1 << " + " << input_2 << " is " << input_1 + input_2 << ".\n";
+std::cout << input_1 << " - " << input_2 << " is " << input_1 - input_2 << ".\n";
+}
